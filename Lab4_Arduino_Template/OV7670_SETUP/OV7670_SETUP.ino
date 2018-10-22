5bf8468edaa157ee43f1b2e8bc28098a56048073
labuser <pdr64@cornell.edu>
2018-10-22 16:29:58 -0400

    lab4

---
+++ Lab4_Arduino_Template/OV7670_SETUP/OV7670_SETUP.ino
@@ -0,0 +1,94 @@
+#include <Wire.h>
+
+#define OV7670_I2C_ADDRESS /*TODO: write this in hex (eg. 0xAB) */
+
+
+///////// Main Program //////////////
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  
+  // TODO: READ KEY REGISTERS
+  
+  delay(100);
+  
+  // TODO: WRITE KEY REGISTERS
+  
+  read_key_registers();
+}
+
+void loop(){
+ }
+
+
+///////// Function Definition //////////////
+void read_key_registers(){
+  /*TODO: DEFINE THIS FUNCTION*/
+}
+
+byte read_register_value(int register_address){
+  byte data = 0;
+  Wire.beginTransmission(OV7670_I2C_ADDRESS);
+  Wire.write(register_address);
+  Wire.endTransmission();
+  Wire.requestFrom(OV7670_I2C_ADDRESS,1);
+  while(Wire.available()<1);
+  data = Wire.read();
+  return data;
+}
+
+String OV7670_write(int start, const byte *pData, int size){
+    int n,error;
+    Wire.beginTransmission(OV7670_I2C_ADDRESS);
+    n = Wire.write(start);
+    if(n != 1){
+      return "I2C ERROR WRITING START ADDRESS";   
+    }
+    n = Wire.write(pData, size);
+    if(n != size){
+      return "I2C ERROR WRITING DATA";
+    }
+    error = Wire.endTransmission(true);
+    if(error != 0){
+      return String(error);
+    }
+    return "no errors :)";
+ }
+
+String OV7670_write_register(int reg_address, byte data){
+  return OV7670_write(reg_address, &data, 1);
+ }
+
+void set_registers(){
+    OV7670_write_register(0x12, 0x82); //COM7: Reset registers, enable color bar
+    OV7670_write_register(0x0C, 0x08); //COM3: Enable scaling
+    OV7670_write_register(0x42, 0x08); //COM17: DSP color bar enable
+    OV7670_write_register(0x40, 0xB0); //COM15: Output range: [00] to [FF]???
+    OV7670_write_register(0x1E, 0x08); //MVFP: Vertically flip image enable
+    OV7670_write_register(0x11, 0x..); //CLKRC: Use external clock directly ???
+}
+
+void set_color_matrix(){
+    OV7670_write_register(0x4f, 0x80);
+    OV7670_write_register(0x50, 0x80);
+    OV7670_write_register(0x51, 0x00);
+    OV7670_write_register(0x52, 0x22);
+    OV7670_write_register(0x53, 0x5e);
+    OV7670_write_register(0x54, 0x80);
+    OV7670_write_register(0x56, 0x40);
+    OV7670_write_register(0x58, 0x9e);
+    OV7670_write_register(0x59, 0x88);
+    OV7670_write_register(0x5a, 0x88);
+    OV7670_write_register(0x5b, 0x44);
+    OV7670_write_register(0x5c, 0x67);
+    OV7670_write_register(0x5d, 0x49);
+    OV7670_write_register(0x5e, 0x0e);
+    OV7670_write_register(0x69, 0x00);
+    OV7670_write_register(0x6a, 0x40);
+    OV7670_write_register(0x6b, 0x0a);
+    OV7670_write_register(0x6c, 0x0a);
+    OV7670_write_register(0x6d, 0x55);
+    OV7670_write_register(0x6e, 0x11);
+    OV7670_write_register(0x6f, 0x9f);
+    OV7670_write_register(0xb0, 0x84);
+}
---
+++ Lab4_FPGA_Template/lab4_inst.v
@@ -0,0 +1,6 @@
+lab4	lab4_inst (
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig ),
+	.c1 ( c1_sig ),
+	.c2 ( c2_sig )
+	);

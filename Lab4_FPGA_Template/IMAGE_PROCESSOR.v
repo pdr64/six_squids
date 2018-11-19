06ecd9e8e8f68c46ed06b218e93b01a3a6858317
kebradford <bradfordkatiee@gmail.com>
2018-11-18 19:21:25 -0500

    Update IMAGE_PROCESSOR.v

--- Lab4_FPGA_Template/IMAGE_PROCESSOR.v
+++ Lab4_FPGA_Template/IMAGE_PROCESSOR.v
@@ -10,7 +10,8 @@ module IMAGE_PROCESSOR (
 	VGA_PIXEL_Y,
 	//VGA_HREF_NEG,
 	VGA_VSYNC_NEG,
-	RESULT
+	RESULT, 
+	CAM_HREF_NEG
 );
 
 
@@ -23,12 +24,9 @@ input 		CLK;
 input [9:0] VGA_PIXEL_X;
 input [9:0] VGA_PIXEL_Y;
 input			VGA_VSYNC_NEG;
-//input [22]  GPIO_1_D;
-//wire        CAM_HREF_NEG;
+input       CAM_HREF_NEG;
 
-//assign CAM_HREF_NEG  = GPIO_1_D[22];
-
-output [2:0] RESULT;
+output[2:0] RESULT;
 
 reg RESULT;
 reg [15:0] countBLUE;
@@ -39,34 +37,36 @@ reg [15:0] B_CNT_THRESHOLD = 16'd20000;
 reg lastsync = 1'b0;
 
 always @(posedge CLK) begin
-	if(CAM_HREF_NEG) begin
-		if(PIXEL_IN == 8'b0) begin
-			countBLUE = countBLUE + 16'd1;
+	if(VGA_PIXEL_X>(`SCREEN_WIDTH/2-40)&& VGA_PIXEL_X<(`SCREEN_WIDTH/2+40) && VGA_PIXEL_Y>(`SCREEN_HEIGHT/2-40)&& VGA_PIXEL_Y<(`SCREEN_HEIGHT/2+40)) begin
+		if(CAM_HREF_NEG) begin
+			if(PIXEL_IN[3:0] ==4'b1111) begin
+				countBLUE = countBLUE + 16'd1;
+			end
+			else if(PIXEL_IN[7:4] ==4'b1111) begin
+				countRED = countRED +16'd1; 
+			end
+			else begin
+				countNULL = countNULL + 16'd1;
+			end
 		end
-		else if(PIXEL_IN[7:5] > 3'b010) begin
-			countRED = countRED +16'b1; 
+		if(VGA_VSYNC_NEG == 1'b1 && lastsync == 1'b0) begin
+			if(countRED > R_CNT_THRESHOLD) begin
+				RESULT = 3'b010;
+			end
+			else if(countBLUE < B_CNT_THRESHOLD) begin 
+				RESULT = 3'b100;
+			end
+			else begin
+				RESULT = 3'b111;
+			end
 		end
-		else begin
-			countNULL = countNULL + 16'd1;
+		if(VGA_VSYNC_NEG == 1'b0 && lastsync == 1'b1) begin
+			countBLUE = 16'b0;
+			countRED  = 16'b0;
+			countNULL = 16'b0;
 		end
+		lastsync = VGA_VSYNC_NEG;
 	end
-	if(VGA_VSYNC_NEG == 1'b1 && lastsync == 1'b0) begin
-		if(countBLUE > B_CNT_THRESHOLD) begin
-			RESULT = 3'b111;
-		end
-		else if(countRED > R_CNT_THRESHOLD) begin 
-			RESULT = 3'b110;
-		end
-		else begin
-			RESULT = 3'b000;
-		end
-	end
-	if(VGA_VSYNC_NEG == 1'b0 && lastsync == 1'b1) begin
-		countBLUE = 16'b0;
-		countRED  = 16'b0;
-		countNULL = 16'b0;
-	end
-	lastsync = VGA_VSYNC_NEG;
 end
 
 endmodule

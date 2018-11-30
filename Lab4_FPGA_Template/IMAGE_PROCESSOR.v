b3e6fd92636e75c204cbc298fa6545e5048e9242
cazadze <42716371+cazadze@users.noreply.github.com>
2018-11-30 10:23:06 -0500

    Update IMAGE_PROCESSOR.v

--- Lab4_FPGA_Template/IMAGE_PROCESSOR.v
+++ Lab4_FPGA_Template/IMAGE_PROCESSOR.v
@@ -36,15 +36,14 @@ reg [9:0] R_CNT_THRESHOLD = 10'd80;
 reg [9:0] B_CNT_THRESHOLD = 10'd80;
 reg lastsync = 1'b0;
 
-reg [9:0] red1;
-reg [9:0] red2;
-reg [9:0] point3;
-reg [9:0] blue1;
-reg [9:0] blue2;
-reg [9:0] blue3;
-reg [9:0] blue4;
-reg [9:0] red4;
-reg [9:0] red3;
+reg [9:0] red1 = 10'b0;
+reg [9:0] red2 = 10'b0;
+reg [9:0] red3 = 10'b0;
+reg [9:0] blue1 = 10'b0;
+reg [9:0] blue2 = 10'b0;
+reg [9:0] blue3 = 10'b0;
+reg [9:0] blue4 = 10'b0;
+reg [9:0] red4 = 10'b0;
 reg [9:0] firstRED;
 reg [9:0] firstBLUE;
 reg colorseen=1'b0;
@@ -52,12 +51,8 @@ reg colorseen=1'b0;
 localparam RED   = 8'b111_000_00;
 localparam BLUE  = 8'b000_000_11;
 
-//assign RESULT = 3'b111;
 always @(posedge CLK) begin
-//  RESULT = 3'b110;
 	if(VGA_PIXEL_X>((`SCREEN_WIDTH/2)-40)&& VGA_PIXEL_X<((`SCREEN_WIDTH/2)+40) && VGA_PIXEL_Y<((`SCREEN_HEIGHT/2)+40)&& VGA_PIXEL_Y>((`SCREEN_HEIGHT/2)-40)) begin
-//   if(!(VGA_PIXEL_X>(`SCREEN_WIDTH-1) || VGA_PIXEL_Y>(`SCREEN_HEIGHT-1))) begin
-		//if(VGA_HREF_NEG) begin
 		if(PIXEL_IN == BLUE) begin
 			countBLUE = countBLUE + 10'd1;
 			if(countBLUE==10'd1) firstBLUE= VGA_PIXEL_Y;	
@@ -69,44 +64,63 @@ always @(posedge CLK) begin
 		else begin
 			countNULL = countNULL + 10'd1;
 		end
-		if((VGA_PIXEL_Y>=firstRED || VGA_PIXEL_Y >=firstBLUE) && (VGA_PIXEL_Y<=firstRED+2 || VGA_PIXEL_Y <=firstBLUE+2))begin
-			blue1 = countBLUE;
-			red1=countRED;
+		
+		if((VGA_PIXEL_Y>firstRED || VGA_PIXEL_Y >firstBLUE) && (VGA_PIXEL_Y<=firstRED+5 || VGA_PIXEL_Y <=firstBLUE+5))begin
+			if(~colorseen)begin
+			blue1 = blue1 +VGA_PIXEL_X;
+			red1=red1 +VGA_PIXEL_X;
+			colorseen=1'b1;
+			end
+			if(VGA_PIXEL_X==((`SCREEN_WIDTH/2)-39))begin
+			colorseen=1'b0;
+			end
 		end
-		else if((VGA_PIXEL_Y>firstRED +4 || VGA_PIXEL_Y >=firstBLUE +4) && (VGA_PIXEL_Y<=firstRED+6 || VGA_PIXEL_Y <=firstBLUE+6))begin
-			blue2 = countBLUE;
-			red2=countRED;
+		else if((VGA_PIXEL_Y>firstRED +5 || VGA_PIXEL_Y >firstBLUE +5) && (VGA_PIXEL_Y<=firstRED+10 || VGA_PIXEL_Y <=firstBLUE+10))begin
+			if(~colorseen)begin
+			blue2 = blue2 +VGA_PIXEL_X;
+			red2=red2 +VGA_PIXEL_X;
+			colorseen=1'b1;
+			end
+			if(VGA_PIXEL_X==((`SCREEN_WIDTH/2)-39))begin
+			colorseen=1'b0;
+			end
 		end
-		else if((VGA_PIXEL_Y>=firstRED +10 || VGA_PIXEL_Y >=firstBLUE +10) && (VGA_PIXEL_Y<=firstRED+12 || VGA_PIXEL_Y <=firstBLUE+12))begin
-			blue3 = countBLUE;
-			red3=countRED;
+		else if((VGA_PIXEL_Y>firstRED +10 || VGA_PIXEL_Y >firstBLUE +10) && (VGA_PIXEL_Y<=firstRED+15 || VGA_PIXEL_Y <=firstBLUE+15))begin
+			if(~colorseen)begin
+			blue3 = blue3 +VGA_PIXEL_X;
+			red3=red3 +VGA_PIXEL_X;
+			colorseen=1'b1;
+			end
+			if(VGA_PIXEL_X==((`SCREEN_WIDTH/2)-39))begin
+			colorseen=1'b0;
+			end
 		end
-		else if((VGA_PIXEL_Y>firstRED + 15|| VGA_PIXEL_Y >=firstBLUE +15) && (VGA_PIXEL_Y<=firstRED+17 || VGA_PIXEL_Y <=firstBLUE+17))begin
-			blue4 = countBLUE;
-			red4=countRED;
+		else if((VGA_PIXEL_Y>firstRED + 15|| VGA_PIXEL_Y >firstBLUE +15) && (VGA_PIXEL_Y<=firstRED+20 || VGA_PIXEL_Y <=firstBLUE+20))begin
+			if(~colorseen)begin
+			blue4 = blue4 +VGA_PIXEL_X;
+			red4=red4 +VGA_PIXEL_X;
+			colorseen=1'b1;
+			end
+			if(VGA_PIXEL_X==((`SCREEN_WIDTH/2)-39))begin
+			colorseen=1'b0;
+			end
 		end			
 		
 	end
 	if(VGA_VSYNC_NEG == 1'b1 && lastsync == 1'b0) begin
-//			if(countRED >countBLUE) begin
-//				RESULT = 3'b010;
-//			end
-//			else if(countBLUE >countRED) begin 
-//				RESULT = 3'b100;
-//			end
-//if it is a red diamond (001), if it is red square (011), if it is red triangle (010)
+		//if it is a red diamond (001), if it is red square (011), if it is red triangle (010)
 		//if it is a blue diamond (100), if it is blue square (110), if it is blue triangle (101)
 		if(countRED >= R_CNT_THRESHOLD) begin
-			if(red1<(red2-red1) && (red4-red3)<(red3-red2)) begin//&& (red2-red1)>(red3-red2)) 
+			if(red1+red2)>(red2+red3) && (red3+red4)>(red2+red3)) begin
 				RESULT= 3'b001; end  // diamond
-			else if(red1<(red2-red1)&& (red4-red3)>(red3-red2)) begin   //&& (red2-red1)<(red3-red2))
+			else if((red1+red2)>(red2+red3)&& (red2+red3)>(red3+red4)) begin  
 				RESULT = 3'b010; end// triangle
-			//else RESULT = 3'b011;                                                //square
+			else RESULT = 3'b011;                                                //square
 		end
 		else if(countBLUE >= B_CNT_THRESHOLD) begin 
-			if(blue1<(blue2-blue1)&& (blue4-blue3)>(blue3-blue2)) RESULT= 3'b100;   // diamond
-			else if(blue1<(blue2-blue1)&& (blue4-blue3)>(blue3-blue2))RESULT = 3'b101; // triangle
-			//else RESULT = 3'b110; 																		//square
+			if(blue1+blue2)>(blue2+blue3) && (blue3+blue4)>(blue2+blue3)) RESULT= 3'b100;   // diamond
+			else if((blue1+blue2)>(blue2+blue3)&& (blue2+blue3)>(blue3+blue4))RESULT = 3'b101; // triangle
+			else RESULT = 3'b110; 																		//square
 		end
 
 		else begin
